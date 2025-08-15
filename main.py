cee3b7e8b04be17305713245173dbc839aa89e25
Yuriy Terlyanskiy <boxtersh@yandex.ru>
2025-08-16 01:41:42 +0300

    New function remove, pop

--- main.py
+++ main.py
@@ -31,10 +31,46 @@ class List:
 
         for i in range(self.__count):
             self.__memory[self.__count - i] = self.__memory[self.__count - 1 - i]
+
         self.__memory[0] = elem
         self.__count += 1
-    # remove(elem) - -
-    # - pop(index)
+
+    def remove(self, elem):
+        if self.__count == 0: return
+
+        target_position = None
+        i = self.__count - 1
+
+        if self.__memory[i] == elem:
+            self.__memory[i] = None
+            self.__count -= 1
+            return
+
+        while i >= 0 or self.__memory[i] != elem:
+            i -= 1
+            if self.__memory[i] == elem:
+                target_position = i
+
+        if target_position != None:
+            for i in range(target_position, self.__count - 1):
+                self.__memory[i] = self.__memory[i + 1]
+
+            self.__memory[self.__count - 1] = None
+            self.__count -= 1
+
+    def pop(self, index):
+        if index > self.__count - 1:
+            return
+
+        return_elem_index = self.__memory[index]
+
+        for i in range(index, self.__count - 1):
+            self.__memory[i] = self.__memory[i + 1]
+
+        self.__memory[self.__count - 1] = None
+        self.__count -= 1
+        return return_elem_index
+
     # - insert(index, elem)
     # - find(elem)
     # count(elem)
