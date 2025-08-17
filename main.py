4b5400526193560c59db4e083e83d60c6eaff310
Yuriy Terlyanskiy <boxtersh@yandex.ru>
2025-08-17 20:56:03 +0300

    New function insert, find, count, clear, lenght, is_empty

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ def realloc(now_memory, N):
     return new_memory
 
 
+def free(now_memory):
+    now_memory = None
+
+
 class List:
 
     def __init__(self):
@@ -71,12 +75,73 @@ class List:
         self.__count -= 1
         return return_elem_index
 
-    # - insert(index, elem)
-    # - find(elem)
-    # count(elem)
-    # clear()
-    # lenght()
-    # - is_empty()
+    def insert(self, index, elem):
+        if index >= self.__count and self.__count + 1 <= self.__memory:
+            self.__memory[self.__count] = elem
+            self.__count += 1
+
+        elif index >= self.__count and self.__count + 1 > self.__memory:
+            self.__memory = realloc(self.__memory, self.__size)
+            self.__memory[self.__count] = elem
+            self.__count += 1
+
+        elif index < self.__count and self.__count + 1 <= self.__memory:
+
+            for i in range(self.__count, index, -1):
+                self.__memory[i] = self.__memory[i - 1]
+
+            self.__memory[index] = elem
+
+        else:
+            self.__memory = realloc(self.__memory, self.__size)
+
+            for i in range(self.__count, index, -1):
+                self.__memory[i] = self.__memory[i - 1]
+
+            self.__memory[index] = elem
+
+    def find(self, elem):
+        if self.__count == 0:
+            return
+
+        if self.__memory[0] == elem:
+            return 0
+
+        i = 0
+        target_position = None
+        while i != self.__count - 1 or self.__memory[i] != elem:
+            i += 1
+            if self.__memory[i] == elem:
+                target_position = i
+
+        if target_position != None:
+            return target_position
+
+    def count(self, elem):
+        count_entries_elem_list = 0
+
+        if self.__count == 0:
+            return count_entries_elem_list
+
+        for lis_item in range(self.__memory):
+            if lis_item == elem:
+                count_entries_elem_list += 1
+
+        return count_entries_elem_list
+
+    def clear(self):
+        free(self.__memory)
+        self.__memory = malloc(5)
+
+    def lenght(self):
+        return self.__count
+
+    def is_empty(self):
+        if self.__count == 0:
+            return True
+
+        return False
+
     # - get_count()
     def get_count(self):
         return self.__count
